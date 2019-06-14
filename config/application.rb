f33099b6c39f19a6921a08e95814314b7d770591
RaceFPV <racefpv@outlook.com>
2019-06-14 04:49:14 +0000

    add some logos, fix fonts and colors, clean up background css

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,6 @@ module Owt
     # the framework and any gems in your application.
     
     config.hosts << "6e9412cf6bb342f7bc538a84417dd0fc.vfs.cloud9.us-east-1.amazonaws.com"
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
