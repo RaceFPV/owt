416b82466be971dae868a5290fc843d55eb95119
RaceFPV <racefpv@outlook.com>
2019-06-24 03:34:34 +0000

    fix some scrape job syntax

--- app/jobs/harmony_roster_scrape_job.rb
+++ app/jobs/harmony_roster_scrape_job.rb
@@ -1,11 +1,8 @@
 class HarmonyRosterScrapeJob < ApplicationJob
   queue_as :default
-  rescue_from(ErrorLoadingSite) do
-    retry_job wait: 5.minutes, queue: :low_priority 
-  end 
 
   def perform(*args)
-    session = GoogleDrive::Session.from_config("config_harmony_roster.json")
-    #ws = session.spreadsheet_by_key("1FeXH7yDokCLiQjcjb14Pxq-gkHhKu49KDB6YZssHqGc").worksheets[0]
+    session = GoogleDrive::Session.from_service_account_key(StringIO.new(ENV['SERVICE_ACCOUNT']))
+    ws = session.spreadsheet_by_key("1FeXH7yDokCLiQjcjb14Pxq-gkHhKu49KDB6YZssHqGc").worksheets[1]
   end
 end
