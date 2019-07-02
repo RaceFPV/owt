86100444ab26adedf1e898fe6bf69553eeba4579
RaceFPV <racefpv@outlook.com>
2019-07-02 03:19:43 +0000

    have scraper also do players, fix a few model database reference bugs

--- app/jobs/discord_roster_scrape_job.rb
+++ app/jobs/discord_roster_scrape_job.rb
@@ -7,8 +7,14 @@ class DiscordRosterScrapeJob < ApplicationJob
     ws = session.spreadsheet_by_key("1FeXH7yDokCLiQjcjb14Pxq-gkHhKu49KDB6YZssHqGc").worksheets
     puts "Found spreadsheet, cycling through tabs...."
     ws.drop(1).each do |x|
+      team_save(x)
+      player_save(x)
+    end
+  end
+  
+  def team_save(x)
       if Team.where(name: x.title).exists?
-        puts "Team already exists, checking for updates"
+        puts "Team #{x.title} already exists, checking for updates"
         team = Team.find_by_name(x.title)
         sr = x[16, 4]
         if !sr.nil? and team.sr.nil?
@@ -23,9 +29,14 @@ class DiscordRosterScrapeJob < ApplicationJob
         team.discordlink ||= x[21, 2]
         team.twittername ||= x[22, 1]
         team.twitterlink ||= x[22, 2]
-        puts "Collected team data... saving"
-        team.save!
-        puts "Save complete"
+        puts "Collected team data..."
+        if team.changed?
+          puts "Team #{x.title} data changed, saving changes..."
+          team.save!
+        else  
+          puts "Team #{x.title} data not changed"
+        end
+        puts "Check for #{x.title} complete"
       else
         puts "Team #{x.title} not found... creating entry"
         team = Team.new
@@ -46,7 +57,48 @@ class DiscordRosterScrapeJob < ApplicationJob
         puts "Save complete"
       end
     end
-  end
+    
+    def player_save(x)
+      (6..15).each do |y|
+        if !x[y, 1].empty?
+          if Player.where(name: x[y, 1]).exists?
+            puts "Player #{x[y, 1]} already exists, checking for updates"
+            player = Player.find_by_name(x[y, 1])
+            sr = x[y, 4]
+            if !sr.nil? and player.sr.nil?
+              player.sr = sr.to_i
+            elsif !sr.nil?
+              player.sr ||= sr.to_i
+            end
+            player.battletag ||= x[y, 2]
+            player.role ||= x[y, 3]
+            player.team_id ||= Team.find_by_name(x.title).id
+            puts "Collected player data..."
+            if player.changed?
+              puts "Player #{x[y, 1]} data changed, saving changes..."
+              team.save!
+            else  
+              puts "Player #{x[y, 1]} data not changed"
+            end
+            puts "Check for #{x[y, 1]} complete"
+          else
+            puts "Player #{x[y, 1]} not found... creating entry"
+            player = Player.new
+            player.name = x[y, 1]
+            sr = x[y, 4]
+            if !sr.nil?
+              player.sr = sr.to_i
+            end
+            player.battletag ||= x[y, 2]
+            player.role ||= x[y, 3]
+            player.team_id ||= Team.find_by_name(x.title).id
+            puts "Collected player data... saving"
+            player.save!
+            puts "Save complete"
+          end
+        end
+      end
+    end
 end
 
 # teams schema
--- app/jobs/harmony_roster_scrape_job.rb
+++ app/jobs/harmony_roster_scrape_job.rb
@@ -7,6 +7,12 @@ class HarmonyRosterScrapeJob < ApplicationJob
     ws = session.spreadsheet_by_key("1oZA-RmtUFRVpo8OS9sNzaK9vRbsihkC9GFgzGvGS-HY").worksheets
     puts "Found spreadsheet, cycling through tabs...."
     ws.drop(1).each do |x|
+      team_save(x)
+      player_save(x)
+    end
+  end
+  
+  def team_save(x)
       if Team.where(name: x.title).exists?
         puts "Team already exists, checking for updates"
         team = Team.find_by_name(x.title)
@@ -23,9 +29,14 @@ class HarmonyRosterScrapeJob < ApplicationJob
         team.discordlink ||= x[21, 2]
         team.twittername ||= x[22, 1]
         team.twitterlink ||= x[22, 2]
-        puts "Collected team data... saving"
-        team.save!
-        puts "Save complete"
+        puts "Collected team data..."
+        if team.changed?
+          puts "Team #{x.title} data changed, saving changes..."
+          team.save!
+        else  
+          puts "Team #{x.title} data not changed"
+        end
+        puts "Check for #{x.title} complete"
       else
         puts "Team #{x.title} not found... creating entry"
         team = Team.new
@@ -46,8 +57,52 @@ class HarmonyRosterScrapeJob < ApplicationJob
         puts "Save complete"
       end
     end
-  end
+    
+    
+    
+    def player_save(x)
+      (6..15).each do |y|
+        if !x[y, 1].empty?
+          if Player.where(name: x[y, 1]).exists?
+            puts "Player #{x[y, 1]} already exists, checking for updates"
+            player = Player.find_by_name(x[y, 1])
+            sr = x[y, 4]
+            if !sr.nil? and player.sr.nil?
+              player.sr = sr.to_i
+            elsif !sr.nil?
+              player.sr ||= sr.to_i
+            end
+            player.battletag ||= x[y, 2]
+            player.role ||= x[y, 3]
+            player.team_id ||= Team.find_by_name(x.title).id
+            puts "Collected player data..."
+            if player.changed?
+              puts "Player #{x[y, 1]} data changed, saving changes..."
+              team.save!
+            else  
+              puts "Player #{x[y, 1]} data not changed"
+            end
+            puts "Check for #{x[y, 1]} complete"
+          else
+            puts "Player #{x[y, 1]} not found... creating entry"
+            player = Player.new
+            player.name = x[y, 1]
+            sr = x[y, 4]
+            if !sr.nil?
+              player.sr = sr.to_i
+            end
+            player.battletag ||= x[y, 2]
+            player.role ||= x[y, 3]
+            player.team_id ||= Team.find_by_name(x.title).id
+            puts "Collected player data... saving"
+            player.save!
+            puts "Save complete"
+          end
+        end
+      end
+    end
 end
+
 
 # teams schema
 #    t.string "name"
