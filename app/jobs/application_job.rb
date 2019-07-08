93c6a27b5ac87309265bb7eb9d9e975aafac86c0
RaceFPV <racefpv@outlook.com>
2019-07-08 03:14:25 +0000

    change scrape interval to 30 minutes to stay within heroku free tier

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -6,6 +6,6 @@ class ApplicationJob < ActiveJob::Base
   # discard_on ActiveJob::
   
   # Enqueue a job to be performed tomorrow at noon.
-  DiscordRosterScrapeJob.set(wait_until: (DateTime.now + 10.minutes)).perform_later
-  HarmonyRosterScrapeJob.set(wait_until: (DateTime.now + 12.minutes)).perform_later
+  DiscordRosterScrapeJob.set(wait_until: (DateTime.now + 30.minutes)).perform_later
+  HarmonyRosterScrapeJob.set(wait_until: (DateTime.now + 31.minutes)).perform_later
 end
